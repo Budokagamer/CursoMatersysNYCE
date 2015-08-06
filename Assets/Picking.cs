911039955238bce04cc3df472c6ce5cdc2f20653
Rckdrigo <rckdigomed@gmail.com>
2015-08-06 00:12:16 -0500

    Correct drag

---
+++ Assets/Picking.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Picking : MonoBehaviour {
+	
+	RaycastHit hit;
+	bool onDrag;
+	Transform objeto;
+
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if(Physics.Raycast(ray,out hit)){
+				print ("Tocaste "+ hit.transform.name + 
+				       " en el punto " + hit.point);
+			//	objeto = hit.transform;
+				onDrag = true;
+			}
+
+		}
+	}
+}
---
+++ Assets/PickingPrimeraVersion.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class PickingPrimeraVersion : MonoBehaviour {
+
+	Vector3 delta;
+	float z;
+
+
+	void Start(){
+		z = Vector3.Distance(Camera.main.transform.position,transform.position);
+	}
+
+	void OnMouseDown(){
+		print ("Sub-zero");
+	}
+
+	
+	void OnMouseDrag(){
+		//print (Camera.main.ScreenPointToRay(Input.mousePosition).origin);
+		transform.position = Camera.main.transform.position + 
+							Camera.main.ScreenPointToRay(Input.mousePosition).direction*z ;
+
+	}
+
+	void OnMouseOver(){
+		print ("Sobre Sub-zero");
+	}
+
+}
