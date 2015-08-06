59b2f707ce3c9e7c820d129bd66301ef04fa96c2
Rckdrigo <rckdigomed@gmail.com>
2015-08-06 11:01:43 -0500

    Luz de estrobo

--- Assets/LightController.cs
+++ Assets/LightController.cs
@@ -3,10 +3,18 @@ using System.Collections;
 
 public class LightController : MonoBehaviour {
 
+	public LightStrobe strobe;
+
 	public Light lamp;
 
-
 	void OnMouseDown(){
+
 		lamp.enabled = !lamp.enabled;
+
+
+		if(strobe.active)
+			strobe.EndStrobe();
+		else
+			strobe.StartStrobe();
 	}
 }
---
+++ Assets/LightStrobe.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightStrobe : MonoBehaviour {
+
+	Light lamp;
+
+	public bool active;
+
+	void Start(){
+		lamp = GetComponent<Light>();
+		active = false;
+		lamp.enabled = false;
+	}
+
+	public void StartStrobe () {
+		StartCoroutine("Strobe");
+		
+		active = true;
+	}
+
+	IEnumerator Strobe(){
+		lamp.enabled = true;
+		yield return new WaitForSeconds(0.1f);
+		lamp.enabled = false;
+		yield return new WaitForSeconds(0.1f);
+		StartCoroutine("Strobe");
+	}
+
+	public void EndStrobe () {
+		active = false;
+		lamp.enabled = false;
+		StopCoroutine("Strobe");
+	}
+
+}
