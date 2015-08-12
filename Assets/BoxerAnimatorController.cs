1fc3fedc0134f154e106083e4a18c5eaddd1e2bd
Rckdrigo <rckdigomed@gmail.com>
2015-08-12 10:18:02 -0500

    Modelos nuevos y blending

---
+++ Assets/BoxerAnimatorController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxerAnimatorController : MonoBehaviour {
+
+	Animator animator;
+	bool isDancing;
+
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		isDancing = false;
+		animator.SetBool("Dancing",isDancing);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.K)){
+			isDancing = !isDancing;
+			animator.SetBool("Dancing",isDancing);
+		}
+	}
+}
